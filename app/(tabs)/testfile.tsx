563128f3da14819c57f7f54ed9c70fd490ae184b
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-05-24 15:30:41 +0500

    testiomg

---
+++ app/(tabs)/testfile.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function testfile() {
+  return (
+    <View>
+      <Text>testfile</Text>
+    </View>
+  )
+}
